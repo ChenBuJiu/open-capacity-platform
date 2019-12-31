4647d226a1ec409f263014c78e3e4a50484d5795
Chenbujiu <695418816@qq.com>
2019-12-31 17:50:37 +0800

    init

---
+++ inner-intergration/db-spring-boot-starter/src/main/java/com/open/capacity/datasource/constant/DataSourceKey.java
@@ -0,0 +1,10 @@
+package com.open.capacity.datasource.constant;
+
+/**
+ * 数据源定义
+ *
+ * @author chenwei
+ */
+public enum DataSourceKey {
+    core, log ,sharding
+}
---
+++ web-portal/back-center/src/main/java/com/open/capacity/web/BackCenterApplication.java
@@ -0,0 +1,19 @@
+package com.open.capacity.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author:chenwei
+ * @Date: 2019/12/31  下午 2:52
+ */
+@Configuration
+@SpringBootApplication
+public class BackCenterApplication {
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(BackCenterApplication.class, args);
+	}
+}
